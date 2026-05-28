da24b51a9c4892645dcfb75663455862f7bd1819
Bryan Buobu <developer@tickideas.org>
2026-05-28 06:57:37 +0100

    feat(player): add playlist sharing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,15 @@
 import { PlaylistBrowser } from "@/components/player/playlist-browser";
 
-export default function Home() {
-  return <PlaylistBrowser />;
+type HomeProps = {
+  searchParams?: Promise<{
+    playlist?: string | string[];
+  }>;
+};
+
+export default async function Home({ searchParams }: HomeProps) {
+  const params = await searchParams;
+  const playlist = params?.playlist;
+  const initialPlaylistId = Array.isArray(playlist) ? playlist[0] : playlist;
+
+  return <PlaylistBrowser initialPlaylistId={initialPlaylistId ?? null} />;
 }
